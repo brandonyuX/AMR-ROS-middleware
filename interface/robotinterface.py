e64dd53a4a9bc673455ff5a7414010054a461df5
nel24 <nelson.goh@m8m.com.sg>
2022-04-11 16:39:35 +0800

    Bug Fix for bootstrap issue, add socketio async data exchange

--- interface/robotinterface.py
+++ interface/robotinterface.py
@@ -2,6 +2,8 @@
 import roslibpy
 import pyodbc
 import sys
+import threading
+
 
 sys.path.append('../Middleware Development')
 
@@ -14,44 +16,102 @@ import interface.dbinterface as dbinterface
 #ros = roslibpy.Ros(host='localhost', port=9090)
 #ros.run()
 
+def get_single_info():
+    client = roslibpy.Ros(host="172.23.44.12", port=8080)
+    client.run()
+    listener = roslibpy.Topic(client, '/robot_pose', 'geometry_msgs/Pose')
+    listener.subscribe(receive_message)
+
 def get_info():
     # Loop through ip in configuration file
+    pos={}
     iplist=dbinterface.getIPList()
-    for ip in iplist:
-        #Subscribe to topic in ros robot
-        client = roslibpy.Ros(host=ip, port=8080)
-        client.run()
-        cmdsrv = roslibpy.Service(client,'/web_cmd','htbot/mqueue')
-        cmdreq=roslibpy.ServiceRequest(values={
-                        cmd : 1,
-                        LP : 0,
-                        GN : 0,
-                        gps : "",
-                        lps : "POWER",
-                            pw : "",
-                        tx : 1.0,
-                        ty : 2.0,
-                        tz : 3.0,
-                        rx : 0.0,
-                            ry : 0.0,
-                        rz : 0.0,
-                        rw : 0.0,
-                        prd : 0.0,
-                            pra : 0.0,
-                        psd : 0.0,
-                        psa : 0.0,
-                        prd1 : 0.0,
-                        pra1 : 0.0,
-                        psd1 : 0.0,
-                        psa1 : 0.0,
-                        align : 0.0,
-                        func : 0.0 
-                    })
-        listener = roslibpy.Topic(client, '/rosout', 'std_msgs/String')
-        listener.subscribe(lambda message: print('rosout: ' + message['data']))
-        
-       
-        
+    client = roslibpy.Ros(host="172.23.44.12", port=8080)
+    client.run()
+    cmdsrv = roslibpy.Service(client,'/web_cmd','htbot/mqueue')
+    # cmdreq=roslibpy.ServiceRequest(values={
+    #                 cmd : 1,
+    #                 LP : 0,
+    #                 GN : 0,
+    #                 gps : "",
+    #                 lps : "POWER",
+    #                     pw : "",
+    #                 tx : 1.0,
+    #                 ty : 2.0,
+    #                 tz : 3.0,
+    #                 rx : 0.0,
+    #                     ry : 0.0,
+    #                 rz : 0.0,
+    #                 rw : 0.0,
+    #                 prd : 0.0,
+    #                     pra : 0.0,
+    #                 psd : 0.0,
+    #                 psa : 0.0,
+    #                 prd1 : 0.0,
+    #                 pra1 : 0.0,
+    #                 psd1 : 0.0,
+    #                 psa1 : 0.0,
+    #                 align : 0.0,
+    #                 func : 0.0 
+    #             })
+    def start_receive_pose():
+        listener = roslibpy.Topic(client, '/robot_pose', 'geometry_msgs/Pose')
+        listener.subscribe(receive_message)
+    
+    t1 = threading.Thread(target=start_receive_pose)
+    t1.start()
+    t1.join()
+   
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+            client.terminate()
+    # for ip in iplist:
+    #     #Subscribe to topic in ros robot
+    #     client = roslibpy.Ros(host=ip, port=8080)
+    #     client.run()
+    #     cmdsrv = roslibpy.Service(client,'/web_cmd','htbot/mqueue')
+    #     cmdreq=roslibpy.ServiceRequest(values={
+    #                     cmd : 1,
+    #                     LP : 0,
+    #                     GN : 0,
+    #                     gps : "",
+    #                     lps : "POWER",
+    #                         pw : "",
+    #                     tx : 1.0,
+    #                     ty : 2.0,
+    #                     tz : 3.0,
+    #                     rx : 0.0,
+    #                         ry : 0.0,
+    #                     rz : 0.0,
+    #                     rw : 0.0,
+    #                     prd : 0.0,
+    #                         pra : 0.0,
+    #                     psd : 0.0,
+    #                     psa : 0.0,
+    #                     prd1 : 0.0,
+    #                     pra1 : 0.0,
+    #                     psd1 : 0.0,
+    #                     psa1 : 0.0,
+    #                     align : 0.0,
+    #                     func : 0.0 
+    #                 })
+    #     listener = roslibpy.Topic(client, '/rosout', 'std_msgs/String')
+    #     listener.subscribe(lambda message: print('rosout: ' + message['data']))
+
+def test_async():
+    pass
+
+def start_async():
+    t1 = threading.Thread(target=test_async)
+    t1.start()
+    t1.join()
+
+    
+def receive_message(message):
+    print(message)       
 
 def publish_info(rid):
     
@@ -95,3 +155,4 @@ def publish_cmd(rid):
 def get_task_list():
     
     pass
+
--- server.py
+++ server.py
@@ -1,16 +1,45 @@
-from flask import Flask, render_template,request
+from cgi import test
+from flask import Flask, render_template,request,session
 import interface.dbinterface as dbinterface
+from mwclass.testclass import testclass
 from mwclass.subtask import SubTask
+from flask_socketio import SocketIO, emit
+from threading import Lock
+
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on installed packages.
+async_mode = None
 
 app = Flask('testapp')
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
 actiondict={'0':'Move','1':'Unload','2':'Load','3':'Custom Command'}
 subtasklist=[]
+tclist=[]
+tc=testclass(0,0,0)
 
+#Background activity to perform
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    
+    while True:
+        socketio.sleep(3)
+        count += 1
+        tc.x=count
+        tc.y=count+1
+        tc.z=count+2
+        forjson=tc.toJSON()
+        socketio.emit('my_response',
+                      {'data': count, 'count': count,'class':forjson})
 
+#Define homepage 
 @app.route('/')
 def index():
     tsklist=dbinterface.getTaskList()
-    return render_template('index.html',tsklist=tsklist)
+    return render_template('index.html',tsklist=tsklist, async_mode=async_mode)
 
 @app.route('/taskmodelquery')
 def taskmodelconfig():
@@ -26,49 +55,79 @@ def taskmodelconfigget():
 def taskmodelcreate():
     return render_template('taskmodelcreate.html')
 
-@app.route("/clearsubtask/", methods=['POST'])
-def clear():
-    print('Run clear routine')
-    subtasklist.clear()
-    return render_template('taskmodelcreate.html')
-
-@app.route("/createtm/", methods=['POST'])
-def createtm():
-    print('Create task model')
-    tskmodno=request.form['tskmodno']
-    querysublist=dbinterface.getSubTaskListByID(int(tskmodno))
-    #print(len(querysublist))
-    #Check if task model id exist
-    if(len(querysublist)==0):
-        dbinterface.writeSubTask(subtasklist)
-        print('Task Model created!')
-    else:
-        print('Task Model already exist, please delete old task model to create new task model')
-    return render_template('taskmodelcreate.html')
 
 
+
+
+# Routes for task create
 @app.route('/taskmodelcreate', methods=['POST'])
 def taskmodelcreatepost():
-    tskmodno=request.form['tskmodno']
-    sel=request.form['gridRadios']
-    if(sel=='CustomCommand'):
-        cmd=request.form['custcmd']
-        print(cmd)
-        print(len(subtasklist))
-        st=SubTask(tskmodno,sel,str(len(subtasklist)+1),cmd)
-        subtasklist.append(st)
-    else:
-        print(tskmodno)
-        print(sel)
-        print(len(subtasklist))
-        st=SubTask(tskmodno,sel,str(len(subtasklist)+1),'')
-        subtasklist.append(st)
+    #Read the type of request and process
+    posttype=request.form['type']
+    print(posttype)
     
-    # st=SubTask(tskmodno,sel,cmd,len(subtasklist+1))
-    # subtasklist.append(st)
+    if(posttype=='createtm'):
+        print('Create task model')
+        tskmodno=request.form['tskmodno']
+        print(tskmodno)
+        querysublist=dbinterface.getSubTaskListByID(int(tskmodno))
+        print(len(querysublist))
+        #Check if task model id exist
+        if(len(querysublist)==0):
+            dbinterface.writeSubTask(subtasklist)
+            print('Task Model created!')
+        else:
+            print('Task Model already exist, please delete old task model to create new task model')
+        return render_template('taskmodelcreate.html')
 
-    return render_template('taskmodelcreate.html',subtsklist=subtasklist,tskmod=tskmodno)
+    if(posttype=="clear"):
+        print('Run clear routine')
+        subtasklist.clear()
+        return render_template('taskmodelcreate.html')
+    if posttype=="addstep":
+        tskmodno=request.form['tskmodno']
+        sel=request.form['gridRadios']
+        if(sel=='CustomCommand'):
+            cmd=request.form['custcmd']
+            print(cmd)
+            print(len(subtasklist))
+            st=SubTask(tskmodno,sel,str(len(subtasklist)+1),cmd)
+            subtasklist.append(st)
+        else:
+            print(tskmodno)
+            print(sel)
+            print(len(subtasklist))
+            st=SubTask(tskmodno,sel,str(len(subtasklist)+1),'')
+            subtasklist.append(st)
 
+        return render_template('taskmodelcreate.html',subtsklist=subtasklist,tskmod=tskmodno)
+
+@socketio.event
+def my_event(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    forjson=tc.toJSON()
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count'],'class': forjson})
+
+# Receive the test request from client and send back a test response
+@socketio.on('test_message')
+def handle_message(data):
+    print('received message: ' + str(data))
+    emit('test_response', {'data': 'Test response sent'})
+
+# Broadcast a message to all clients
+@socketio.on('broadcast_message')
+def handle_broadcast(data):
+    print('received: ' + str(data))
+    emit('broadcast_response', {'data': 'Broadcast sent'}, broadcast=True)
+
+@socketio.event
+def connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(background_thread)
+    emit('my_response', {'data': 'Connected', 'count': 0})
 
 if __name__ == '__main__':
     app.run()
