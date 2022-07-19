01c8f4c4ef3d4b9e5de9c181e56457def8f30180
nel24 <nelson.goh@m8m.com.sg>
2022-07-19 09:17:46 +0800

    Minor changes to wording

--- test_wo.py
+++ test_wo.py
@@ -12,7 +12,6 @@ def start_wo():
         dbinterface.updateWO(wo_id,j,stat)
         print('Set station {} for work order {} to status {}'.format(j,wo_id,stat))
         for i in range(1,7):
-
             print('Pushing {} to station {}'.format(dbinterface.getWOQ(i),i))
         time.sleep(1)
 #start_wo()
