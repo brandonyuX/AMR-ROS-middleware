7dbda245031037785617f72d658a6eac9d274577
wreako24 <wreako24@gmail.com>
2023-03-09 14:09:07 +0800

    add amr page to server

---
+++ static/js/amr-control.js
@@ -0,0 +1,11 @@
+// Adjust msmsgblock to the center
+document.getElementById("msmsgblock").innerHTML = '<div class="h6 py-2 fw-bold" style="text-align: center; margin-bottom: 0px;">Master Scheduler <br /><div id="msmsg"></div></div>'
+document.getElementById("msmsgblock").style.position = "relative";
+dif = (document.getElementById("userLogout").offsetWidth - document.getElementById("sideToggle").offsetWidth) / 2
+document.getElementById("msmsgblock").style.left = dif + "px";
+document.getElementById("sideDbBtn").classList.add("active");
+
+
+
+setInterval(get_table, 3000);
+get_table();
